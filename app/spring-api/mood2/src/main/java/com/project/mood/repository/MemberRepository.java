5c13c42e0a0e11faa9f9c54eae94d0212ea14e3b
2sseong <sh999051@naver.com>
2025-07-16 17:24:29 +0900

    파일 삭제

--- app/spring-api/mood2/src/main/java/com/project/mood/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.mood.repository;
-
-import com.project.mood.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    boolean existsByUserId(String userId);
-}
