7db06027188b9a853d6f2780a88c3738464d8a41
oatmealflyer <rudwnzlxl6@naver.com>
2025-07-17 17:29:59 +0900

    0717 오후
    0717 퇴근

--- app/react-app/moodlog/src/pages/MainPage.jsx
+++ app/react-app/moodlog/src/pages/MainPage.jsx
@@ -1,8 +1,6 @@
 import '../App.css';
 import { useNavigate, useLocation  } from 'react-router-dom';
-import React, { useState } from 'react'; 
-import { useNavigate, useLocation } from 'react-router-dom';
-import { FaSearch } from 'react-icons/fa';
+// import { FaSearch } from 'react-icons/fa';
 import React, { useEffect, useState, useContext } from 'react'; // ✔ 상태 관리를 위해 필요
 import LoginModal from '../components/LoginModal'; // ✔ 모달 컴포넌트 가져오기
 import { UserContext } from '../context/UserContext';
@@ -32,17 +30,6 @@ function MainPage({ isLoggedIn, setIsLoggedIn }) {
               <div></div>
             </div>
           </div>
-
-          {/* ✅ 로그인 버튼 추가 */}
-          {userInfo ? (
-            <>
-              <div className="profile">{userInfo.nickname}님</div>
-              <button onClick={logout} className="login-btn">로그아웃</button>
-            </>
-          ) : (
-            <button className="login-btn" onClick={() => setShowModal(true)}>로그인</button>
-          )}
-          <div className="profile">👤</div>
         </div>
 
         <h3>{isLoggedIn ? '나만의 Mood Picks' : 'Mood Picks'}</h3>
